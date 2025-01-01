7c59ec1807f932fd0f3f60ffc9bbedd157f29d19
Neph <nephcohen@gmail.com>
2025-01-01 14:45:28 +0100

    Script foundry
    Ca ne fonctionne pas avec cette version, je dois revoir la MAJ 2025

--- script/SimpleStorage.s.sol
+++ script/SimpleStorage.s.sol
@@ -5,14 +5,25 @@ import "forge-std/Script.sol";
 import "../src/SimpleStorage.sol";
 
 contract MyScript is Script {
-   
-   
     function run() external {
-        uint256 deployerPrivateKey =  vm.envUint("WPK");
-        vm.startBroadcast(deployerPrivateKey);
-
-        SimpleStorage simpleStorage = new SimpleStorage(3);
+        // Récupération de la clé privée depuis la variable d'environnement
+        string memory deployerPrivateKey = vm.envString("WPK");
         
+        // Ajout de "0x" si ce n'est pas présent
+        if (bytes(deployerPrivateKey).length > 0 && bytes(deployerPrivateKey)[0] != "0x") {
+            deployerPrivateKey = string(abi.encodePacked("0x", deployerPrivateKey));
+        }
+        
+        uint256 privateKeyUint = vm.parseUint(deployerPrivateKey);
+
+        // Démarrage de la diffusion (broadcast) avec la clé privée
+        vm.startBroadcast(privateKeyUint);
+
+        // Déploiement du contrat SimpleStorage avec un argument de constructeur
+        SimpleStorage simpleStorageInstance = new SimpleStorage(3);
+        console.log("SimpleStorage deployed at:", address(simpleStorageInstance));
+
+        // Arrêt de la diffusion
         vm.stopBroadcast();
     }
 }
